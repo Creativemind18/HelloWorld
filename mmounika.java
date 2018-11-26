b26a5f983f7b546de50bc5b673303d00e738530d
mounikamaddala <mounikamaddala97@gmail.com>
2018-11-26 22:11:31 +0530

    modified file

--- mmounika.java
+++ mmounika.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 predefined package
 user defined package
 public class MyApp{
@@ -12,19 +12,3 @@ method(){
 }
 
 
-=======
-predefined package
-user defined package
-public class MyApp{
-{
-static block optional
-}
-//if need instance then instance data
-//if need static then static data
-method(){
-//members of method
-}
-
-
->>>>>>> 0231c068335ed8390667293bc1d10cae2d2b9a12
-}
