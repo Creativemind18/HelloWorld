2248af8686ebfb08b16a6e7e891e1b57662bbb91
mounikamaddala <mounikamaddala97@gmail.com>
2018-11-26 21:52:53 +0530

    this is an java file

---
+++ mmounika.java
@@ -0,0 +1,14 @@
+predefined package
+user defined package
+public class MyApp{
+{
+static block optional
+}
+//if need instance then instance data
+//if need static then static data
+method(){
+//members of method
+}
+
+
+}
